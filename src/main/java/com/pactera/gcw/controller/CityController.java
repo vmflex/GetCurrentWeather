d9d5f3699cd0711c1bd88306182b571eb1f0d784
LiuJun <j_liu@dreamarts.co.jp>
2019-03-10 23:04:59 +0800

    Add UI and Rest API Doc

--- src/main/java/com/pactera/gcw/controller/CityController.java
+++ src/main/java/com/pactera/gcw/controller/CityController.java
@@ -14,6 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pactera.gcw.dao.model.GcwCity;
 import com.pactera.gcw.service.ICityService;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+
 /**
  * @author dac
  *
@@ -24,16 +28,27 @@ public class CityController {
     @Autowired
     private ICityService service;
 
-    @RequestMapping("/city/list")
+    /**
+     * Get all available cities
+     * @return
+     */
+    @ApiOperation(value = "Get All cities", notes = "Get all available cities from Database.")
+    @RequestMapping(value = "/city/list", method = RequestMethod.GET)
     public List<GcwCity> getAllCities() {
         return this.service.getAllGcwCities();
     }
 
-    @RequestMapping("/city/{code}")
+    @ApiOperation(value = "Get a city by a city code", notes = "Get a city by a city code.")
+    @ApiImplicitParam(name = "code", value = "City Code", required = true, dataType = "String", paramType = "path")
+    @RequestMapping(value = "/city/{code}", method = RequestMethod.GET)
     public GcwCity getCityByCode(@PathVariable("code") String code) {
         return this.service.getGcwCity(code);
     }
 
+    @ApiOperation(value = "Create a City", notes = "Create a city by city code and city name.")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "code", value = "City Code", required = true, dataType = "String"),
+            @ApiImplicitParam(name = "name", value = "City Name", required = true, dataType = "String") })
     @RequestMapping(value = "/city/save", method = RequestMethod.POST)
     public GcwCity saveCity(String code, String name) {
         GcwCity city = new GcwCity();
@@ -43,6 +58,8 @@ public class CityController {
         return city;
     }
 
+    @ApiOperation(value = "Delete a city.", notes = "Delete a city based on city code.")
+    @ApiImplicitParam(name = "code", value = "City Code", required = true, dataType = "String", paramType = "path")
     @RequestMapping(value = "/city/del/{code}", method = RequestMethod.DELETE)
     public boolean deleteCity(@PathVariable("code") String code) {
         return this.service.deleteGcwCity(code);
--- src/main/java/com/pactera/gcw/controller/GetWeatherController.java
+++ src/main/java/com/pactera/gcw/controller/GetWeatherController.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pactera.gcw.dao.model.WeatherModel;
 import com.pactera.gcw.service.IGetWeatherService;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import springfox.documentation.annotations.ApiIgnore;
+
 @RestController
 public class GetWeatherController {
 
@@ -18,13 +22,22 @@ public class GetWeatherController {
     @Autowired
     private IGetWeatherService service;
 
+    /**
+     * Get Weather info based on city code.
+     * @param code city code
+     * @return
+     */
+    @ApiOperation(value = "Get Current Weather Info.", notes = "Get Current Weather Info based on city code.")
     @RequestMapping(value = "/weatherinfo/{code}", method = RequestMethod.GET, produces = "text/plain;charset=utf-8")
+    @ApiImplicitParam(name = "code", value = "City Code", required = true, dataType = "String", paramType = "path")
     public String getWeatherInfo(@PathVariable("code") String code) {
         String weather = service.getWeather(code);
         return weather;
     }
 
     @RequestMapping(value = "/weatherinfo2/{code}", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
+    @Deprecated
+    @ApiIgnore
     public WeatherModel getWeatherInfoAsBean(@PathVariable("code") String code) {
         WeatherModel weather = service.getWeatherBean(code);
         return weather;
